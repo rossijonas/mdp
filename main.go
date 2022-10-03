93aed5d2958bd025a0cb052966be6209e4f4d79e
rossijonas <rossijonas@pm.me>
2022-10-03 16:28:56 -0300

    output a temporary file

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"os"
-	"path/filepath"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
@@ -37,13 +37,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := run(*filename); err != nil {
+	if err := run(*filename, os.Stdout); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-func run(filename string) error {
+func run(filename string, out io.Writer) error {
 	// Read all data from the file and check for errors
 	input, err := os.ReadFile(filename)
 	if err != nil {
@@ -52,8 +52,18 @@ func run(filename string) error {
 
 	htmlData := parseContent(input)
 
-	outName := fmt.Sprintf("%s.html", filepath.Base(filename))
-	fmt.Println(outName)
+	// Create temporary file and check for errors
+	temp, err := os.CreateTemp("", "mdp*.html")
+	if err != nil {
+		return err
+	}
+	if err := temp.Close(); err != nil {
+		return err
+	}
+
+	outName := temp.Name()
+
+	fmt.Fprintln(out, outName)
 
 	return saveHTML(outName, htmlData)
 }
